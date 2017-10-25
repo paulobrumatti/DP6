309799fd9bccdc4e9602877d3f7c8de6ce967bb5
taksqth <gabriel.takeshi.higa@gmail.com>
2017-10-25 21:27:25 -0200

    Inicio da implementação do teste de deeplinks

--- app/src/main/java/br/com/dp6/datascience/GTMHelper.java
+++ app/src/main/java/br/com/dp6/datascience/GTMHelper.java
@@ -2,8 +2,16 @@ package br.com.dp6.datascience;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
+import com.google.firebase.dynamiclinks.PendingDynamicLinkData;
 
 public class GTMHelper {
     public static FirebaseAnalytics firebaseAnalytics;
@@ -37,6 +45,35 @@ public class GTMHelper {
         bundle.putString("eventValue", String.valueOf(value));
         bundle.putString("eventNoInteraction", noInteraction ? "true" : "false");
         firebaseAnalytics.logEvent("Interaction", bundle);
+    }
+
+    public static void getDynamicLink(Intent intent, Activity activity) {
+        FirebaseDynamicLinks.getInstance()
+                .getDynamicLink(intent)
+                .addOnSuccessListener(activity, new OnSuccessListener<PendingDynamicLinkData>() {
+                    @Override
+                    public void onSuccess(PendingDynamicLinkData pendingDynamicLinkData) {
+                        // Get deep link from result (may be null if no link is found)
+                        Uri deepLink = null;
+                        if (pendingDynamicLinkData != null) {
+                            deepLink = pendingDynamicLinkData.getLink();
+                        }
+
+
+                        // Handle the deep link. For example, open the linked
+                        // content, or apply promotional credit to the user's
+                        // account.
+                        // ...
+
+                        // ...
+                    }
+                })
+                .addOnFailureListener(activity, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Log.w(TAG, "getDynamicLink:onFailure", e);
+                    }
+                });
     }
 }
 
