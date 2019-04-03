08aaa781acb04f252b7727ef1b7fede5f62277b7
Paulo Brumatti <paulo.brumatti@dp6.com.br>
2019-04-03 12:18:23 -0300

    Update Gradle

--- app/src/main/java/br/com/dp6/datascience/GTMHelper.java
+++ app/src/main/java/br/com/dp6/datascience/GTMHelper.java
@@ -3,17 +3,22 @@ package br.com.dp6.datascience;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.analytics.FirebaseAnalytics.Event;
+import com.google.firebase.analytics.FirebaseAnalytics.Param;
 
-public class GTMHelper {
-    public static FirebaseAnalytics firebaseAnalytics;
+import java.util.ArrayList;
 
-    public static void initiateFirebase(Context context) {
+class GTMHelper {
+    private static FirebaseAnalytics firebaseAnalytics;
+
+    static void initiateFirebase(Context context) {
         firebaseAnalytics = FirebaseAnalytics.getInstance(context);
         firebaseAnalytics.setAnalyticsCollectionEnabled(true);
     }
 
-    public static void pushScreenview(Activity activity, String screenName) {
+    static void pushScreenview(Activity activity, String screenName) {
         Bundle bundle = new Bundle();
         bundle.putString("screenName", screenName);
         firebaseAnalytics.logEvent("ScreenView", bundle);
@@ -21,15 +26,15 @@ public class GTMHelper {
 
     }
 
-    public static void pushEvent(String category, String action, String label) {
+    static void pushEvent(String category, String action, String label) {
         pushEvent(category, action, label, 0, false);
     }
 
-    public static void pushEvent(String category, String action, String label, int value) {
+    static void pushEvent(String category, String action, String label, int value) {
         pushEvent(category, action, label, value, false);
     }
 
-    public static void pushEvent(String category, String action, String label, int value, boolean noInteraction) {
+    static void pushEvent(String category, String action, String label, int value, boolean noInteraction) {
         Bundle bundle = new Bundle();
         bundle.putString("eventCategory", category);
         bundle.putString("eventAction", action);
@@ -37,6 +42,36 @@ public class GTMHelper {
         bundle.putString("eventValue", String.valueOf(value));
         bundle.putString("eventNoInteraction", noInteraction ? "true" : "false");
         firebaseAnalytics.logEvent("Interaction", bundle);
+    }
+
+    static void pushEcommerce() {
+        Bundle product1 = new Bundle();
+        product1.putString(Param.ITEM_ID, "sku1234"); // ITEM_ID or ITEM_NAME is required
+        product1.putString(Param.ITEM_NAME, "Donut Friday Scented T-Shirt");
+        product1.putString(Param.ITEM_CATEGORY, "Apparel/Men/Shirts");
+        product1.putString(Param.ITEM_VARIANT, "Blue");
+        product1.putString(Param.ITEM_BRAND, "Google");
+        product1.putDouble(Param.PRICE, 29.99);
+        product1.putString(Param.CURRENCY, "USD"); // Item-level currency unused today
+        product1.putString("dimension1", "USD"); // Item-level currency unused today
+        product1.putDouble("metric1", 1.0); // Item-level currency unused today
+        product1.putLong(Param.QUANTITY, 1);
+
+        ArrayList items = new ArrayList();
+        items.add(product1);
+
+        Bundle ecommerceBundle = new Bundle();
+        ecommerceBundle.putParcelableArrayList("items", items);
+
+        ecommerceBundle.putString(Param.TRANSACTION_ID, "T12345");
+        ecommerceBundle.putString(Param.AFFILIATION, "Google Store - Online");
+        ecommerceBundle.putDouble(Param.VALUE, 37.39); // Revenue
+        ecommerceBundle.putDouble(Param.TAX, 2.85);
+        ecommerceBundle.putDouble(Param.SHIPPING, 5.34);
+        ecommerceBundle.putString(Param.CURRENCY, "USD");
+        ecommerceBundle.putString(Param.COUPON, "SUMMER2017");
+
+        firebaseAnalytics.logEvent(Event.ECOMMERCE_PURCHASE, ecommerceBundle);
     }
 }
 
--- app/src/main/java/br/com/dp6/datascience/LoginActivity.java
+++ app/src/main/java/br/com/dp6/datascience/LoginActivity.java
@@ -48,10 +48,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        GTMHelper.pushEcommerce();
         GTMHelper.pushScreenview(this, SCREEN_NAME);
 
         mLoginView = (EditText) findViewById(R.id.login);
-        mPasswordView = (EditText) findViewById(R.id.password);
+        mPasswordView = findViewById(R.id.password);
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
 
@@ -83,7 +84,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        Button mEmailSignInButton = findViewById(R.id.email_sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -163,32 +164,25 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
         // for very easy animations. If available, use these APIs to fade-in
         // the progress spinner.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+        int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
 
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-                }
-            });
+        mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        mLoginFormView.animate().setDuration(shortAnimTime).alpha(show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            }
+        });
 
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mProgressView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-                }
-            });
-        } else {
-            // The ViewPropertyAnimator APIs are not available, so simply show
-            // and hide the relevant UI components.
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-        }
+        mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+        mProgressView.animate().setDuration(shortAnimTime).alpha(
+                show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            }
+        });
+
     }
 
     @Override
